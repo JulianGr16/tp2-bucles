90076f1b8065944986f161800f6f6ca516fb25ff
JulianGaray16 <juliangaray.2116@gmail.com>
2024-07-16 21:01:48 -0300

    se pudo realizar correctamente el ejercicio-1

---
+++ js/ejercicio-01.js
@@ -0,0 +1,8 @@
+const edad = parseInt(prompt('ingrese su edad:'));
+
+if(edad >= 18){
+    document.write('si tiene edad suficiente para conducir');
+}
+else{
+    document.write('no tiene edad suficiente para conducir');
+}
